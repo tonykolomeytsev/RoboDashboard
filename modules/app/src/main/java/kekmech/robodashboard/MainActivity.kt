5bf2c401f37591927d7411d21b38987d0c796868
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2021-10-31 23:28:37 +0300

    Remove themes and colors from xml resources

--- modules/app/src/main/java/kekmech/robodashboard/MainActivity.kt
+++ modules/app/src/main/java/kekmech/robodashboard/MainActivity.kt
@@ -7,26 +7,30 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             MainScreen()
         }
     }
 
-    @Preview(
-        name = "Pixel C",
-        backgroundColor = 0xffffffff,
-        device = Devices.PIXEL_C
-    )
+    @Preview(device = Devices.PIXEL_C)
     @Composable
     fun MainScreen() {
         Box(
@@ -34,7 +38,11 @@ class MainActivity : AppCompatActivity() {
                 .fillMaxSize()
                 .background(Color.White)
         ) {
-            Text(text = "Hello world", modifier = Modifier.align(Alignment.Center))
+            val appName = stringResource(R.string.app_name)
+            Text(
+                text = "Hello $appName!",
+                modifier = Modifier.align(Alignment.Center)
+            )
         }
     }
 }
