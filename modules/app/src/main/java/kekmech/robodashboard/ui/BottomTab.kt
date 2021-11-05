b302dc6b2123508392544a57d4fce3bc0d72c51c
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2021-11-05 20:50:03 +0300

    Fix rail navigation

--- modules/app/src/main/java/kekmech/robodashboard/ui/BottomTab.kt
+++ modules/app/src/main/java/kekmech/robodashboard/ui/BottomTab.kt
@@ -1,8 +1,10 @@
 package kekmech.robodashboard.ui
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.BugReport
 import androidx.compose.material.icons.filled.Dashboard
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.ViewList
 import androidx.compose.ui.graphics.vector.ImageVector
 
 enum class BottomTab(
@@ -13,6 +15,16 @@ enum class BottomTab(
     DASHBOARD(
         title = "Dashboard",
         icon = Icons.Filled.Dashboard
+    ),
+
+    DEBUGGER(
+        title = "Debugger",
+        icon = Icons.Filled.BugReport
+    ),
+
+    ROBOT_LIST(
+        title = "Robots List",
+        icon = Icons.Filled.ViewList
     ),
 
     SETTINGS(
--- modules/app/src/main/java/kekmech/robodashboard/ui/MainScreen.kt
+++ modules/app/src/main/java/kekmech/robodashboard/ui/MainScreen.kt
@@ -2,15 +2,20 @@ package kekmech.robodashboard.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.material.Icon
+import androidx.compose.material.NavigationRail
+import androidx.compose.material.NavigationRailItem
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kekmech.common_navigation.compose.ComposeScreen
 import kekmech.feature_dashboard.DashboardScreenApi
+import kekmech.robodashboard.R
 import kekmech.ui_kit.color.LocalAppColors
 import kekmech.ui_kit.theme.AppTheme
 import kotlinx.parcelize.Parcelize
@@ -20,35 +25,48 @@ class MainScreen : ComposeScreen("Main") {
 
     @Composable
     override fun Content() {
-        Box(
+        Row(
             modifier = Modifier
                 .fillMaxSize()
                 .background(LocalAppColors.current.background100)
         ) {
             var selectedTab by remember { mutableStateOf(BottomTab.DASHBOARD) }
             NavigationRail(
-                modifier = Modifier
-                    .align(Alignment.BottomEnd)
-                    .width(80.dp),
-                backgroundColor = LocalAppColors.current.background100
-            ) {
-                for (tab in BottomTab.values()) {
-                    NavigationRailItem(
-                        selected = selectedTab == tab,
-                        onClick = { selectedTab = tab },
-                        icon = { Icon(tab.icon, tab.title) },
-                        alwaysShowLabel = false,
-                        selectedContentColor = LocalAppColors.current.accentDefault,
-                        unselectedContentColor = LocalAppColors.current.content100,
-                        label = { Text(tab.title, modifier = Modifier.padding(top = 8.dp)) }
-                    )
-                }
-            }
-            Box(modifier = Modifier.padding(end = 80.dp)) {
+                selectedTab = selectedTab,
+                onSelectTab = { selectedTab = it }
+            )
+            Box {
                 when (selectedTab) {
                     BottomTab.DASHBOARD -> DashboardScreenApi.newInstance().Content()
                     BottomTab.SETTINGS -> Unit
                 }
+            }
+        }
+    }
+
+    @Composable
+    private fun NavigationRail(
+        selectedTab: BottomTab,
+        onSelectTab: (BottomTab) -> Unit,
+        modifier: Modifier = Modifier
+    ) {
+        NavigationRail(
+            modifier = modifier.width(80.dp),
+            backgroundColor = LocalAppColors.current.background100,
+            header = {
+                Icon(painterResource(R.drawable.ic_launcher_background), "")
+            }
+        ) {
+            for (tab in BottomTab.values()) {
+                NavigationRailItem(
+                    selected = selectedTab == tab,
+                    onClick = { onSelectTab.invoke(tab) },
+                    icon = { Icon(tab.icon, tab.title) },
+                    alwaysShowLabel = false,
+                    selectedContentColor = LocalAppColors.current.accentDefault,
+                    unselectedContentColor = LocalAppColors.current.content100,
+                    label = { Text(tab.title, modifier = Modifier.padding(top = 8.dp)) }
+                )
             }
         }
     }
